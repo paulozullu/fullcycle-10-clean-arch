5230e149616456f4d63a5082c5d9eee8b88337be
Paulo Fabrício <paulozullu@gmail.com>
2026-06-26 14:51:56 -0300

    feat: implementar caso de uso e DTO para criação de produto com testes unitários

--- src/domain/product/factory/product.factory.ts
+++ src/domain/product/factory/product.factory.ts
@@ -8,7 +8,7 @@ export default class ProductFactory {
     type: string,
     name: string,
     price: number
-  ): ProductInterface {
+  ): Product  | ProductB {
     switch (type) {
       case "a":
         return new Product(uuid(), name, price);
--- src/domain/product/repository/product-repository.interface.ts
+++ src/domain/product/repository/product-repository.interface.ts
@@ -1,5 +1,6 @@
 import RepositoryInterface from "../../@shared/repository/repository-interface";
 import Product from "../entity/product";
+import ProductB from "../entity/product-b";
 
 export default interface ProductRepositoryInterface
-  extends RepositoryInterface<Product> {}
+  extends RepositoryInterface<Product | ProductB> {}
