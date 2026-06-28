2cd77345fcb1c78db47fe166ffd007e8c678f2de
Paulo Fabrício <paulozullu@gmail.com>
2026-06-28 14:24:18 -0300

    feat: implementar casos de uso e DTO para encontrar produto e cliente com testes de unidade e integração

---
+++ src/usecase/product/find/find.product.unity.spec.ts
@@ -0,0 +1,40 @@
+import Product from '../../../domain/product/entity/product';
+import FindProductUseCase from './find.product.usecase';
+
+const product = new Product('123', 'John Doe', 100);
+
+const MockRepository = () => {
+  return {
+    find: jest.fn().mockResolvedValue(product),
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+  };
+};
+
+describe('Unit Test find product use case', () => {
+  it('should find a product', async () => {
+    const productRepository = MockRepository();
+    const usecase = new FindProductUseCase(productRepository);
+
+    const input = { id: '123' };
+    const output = {
+      id: '123',
+      name: 'John Doe',
+      price: 100,
+    };
+    const result = await usecase.execute(input);
+    expect(result).toEqual(output);
+  });
+
+  it('should not find a product', async () => {
+    const productRepository = MockRepository();
+    productRepository.find.mockRejectedValue(new Error('Product not found'));
+    const usecase = new FindProductUseCase(productRepository);
+
+    const input = { id: '123' };
+    expect(() => {
+      return usecase.execute(input);
+    }).rejects.toThrow(new Error('Product not found'));
+  });
+});
